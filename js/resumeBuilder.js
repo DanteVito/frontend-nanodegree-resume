7aa8771fa757953d170024ccb5cecdd5978fba29
DanteVito <dantevitobeagland@gmail.com>
2017-04-11 14:15:03 -0300

    replace methods, append and prepend

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,23 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var myName = "Dante";
+var formattedName = HTMLheaderName.replace("%data%", myName);
+
+
+var myRole = "web develloper";
+var formattedRole = HTMLheaderRole.replace("%data%", myRole);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var invert = function(word){
+	var result = "";
+	for (i=word.length; i>=0; i--){
+		result = result.concat(word[i]);
+	}
+
+	return result
+}
+
+console.log(invert('vinicius'));
