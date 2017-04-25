8886d8c3269ac3bb168da7404794bb5d6fdc4a90
DanteVito <dantevitobeagland@gmail.com>
2017-04-25 15:29:50 -0300

    Add files via upload

--- js/helper.js
+++ js/helper.js
@@ -12,24 +12,24 @@ Cameron Pittman
 These are HTML strings. As part of the course, you'll be using JavaScript functions
 replace the %data% placeholder text you see in them.
 */
-var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderName = '<h1 id="name" class="col-xs-8 col-sm-4 col-md-3">%data%</h1>';
+var HTMLheaderRole = '<span class="col-xs-4 col-sm-8 col-md-9 role-style">%data%</span>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="row flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="col-xs-12 col-sm-4 col-md-2 flex-box mobile-contact"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="col-xs-6 col-sm-4 col-md-2 flex-box contact-sm"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="col-xs-12 col-sm-4 col-md-2 flex-box"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="col-xs-6 col-sm-4 col-md-2 flex-box contact-sm"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLblog = '<li class="col-xs-12 col-sm-4 col-md-2 flex-box"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="col-xs-12 col-sm-4 col-md-2 flex-box contact-sm"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var HTMLbioPic = '<img src="%data%" class="col-sm-4 col-md-4 biopic">';
+var HTMLwelcomeMsg = '<span class="col-sm-8 col-md-8 welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
+var HTMLskillsStart = '<h3 id="skills-h3" class="col-sm-12 col-md-8">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
-var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkStart = '<div class="col-xs-12 col-md-12 work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
@@ -37,26 +37,26 @@ var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
-var HTMLprojectDates = '<div class="date-text">%data%</div>';
-var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectTitle = '<a href="#" class="col-xs-12 col-md-12">%data%</a>';
+var HTMLprojectDates = '<div class="col-xs-12 col-md-12 date-text">%data%</div>';
+var HTMLprojectDescription = '<p class="col-xs-12 col-md-12"><br>%data%</p>';
+var HTMLprojectImage = '<img src="%data%" class="col-xs-6 col-md-6 project-image">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolName = '<a href="#" class="col-xs-12 col-md-12">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
-var HTMLschoolDates = '<div class="date-text">%data%</div>';
-var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolDates = '<div class="col-xs-12 col-md-12 date-text">%data%</div>';
+var HTMLschoolLocation = '<div class="col-xs-12 col-md-12 location-text">%data%</div>';
+var HTMLschoolMajor = '<div class="col-xs-12 col-md-12"><em>Major: %data%</em></div>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineClasses = '<h3 class="col-xs-12 col-md-12 "style="padding-left:0px;">Online Classes</h3>';
+var HTMLonlineTitle = '<a href="#" class="col-xs-12 col-md-12">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineDates = '<div class="col-xs-12 col-md-12 date-text">%data%</div>';
+var HTMLonlineURL = '<div class="col-xs-12 col-md-12"><a href="#" class="online-url">%data%</a></div>';
 
 var internationalizeButton = '<button>Internationalize</button>';
-var googleMap = '<div id="map"></div>';
+var googleMap = '<div id="map" class="col-xs-12 col-md-12"></div>';
 
 
 /*
@@ -86,7 +86,7 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  logClicks(loc.pageX, loc.pageY);
 });
 
 
@@ -178,6 +178,7 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -239,11 +240,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,179 +9,256 @@ var bio = {
 		"email": "dante@vito.com",
 		"github": "dantevito",
 		"twitter": "dantevito",
-		"location": "beagland island"
+		"blog": "myblog.dantevito",
+		"location": "curitiba - brazil"
 	},
-	"welcome": "Welcome to me page! I am a very smart dog, trust me!",
+	"welcomeMessage": "Welcome to my page! I am a very smart dog, trust me! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sem neque, maximus viverra sapien eu, dapibus vehicula leo. Aenean tempor est quis dolor dapibus finibus. In hac habitasse platea dictumst. ",
 	"skills": ["Advanced programming", "Problem solver", "Heuristic thinking"],
-	"pictureUrl": "images/dogbert.jpg"
-}
+	"biopic": "images/dogbert.jpg"
+};
+bio.display = function(){
+	// name and role
+	var myHTMLheaderName = HTMLheaderName.replace("%data%", bio.name);
+	var myHTMLheaderRole = HTMLheaderRole.replace("%data%", "web develloper");
+	var myHTMLbioPic = HTMLbioPic.replace("%data%",bio.biopic);
+	var myHTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+	// contacts
+	var myHTMLmobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	var myHTMLemail = HTMLemail.replace("%data%", bio.contacts.email);
+	var myHTMLgithub = HTMLgithub.replace("%data%",bio.contacts.github);
+	var myHTMLtwitter = HTMLtwitter.replace("%data%",bio.contacts.twitter);
+	var myHTMLblog = HTMLblog.replace("%data%",bio.contacts.blog);
+	var myHTMLlocation = HTMLlocation.replace("%data%",bio.contacts.location);
+
+	var myHTMLmobileTop = myHTMLmobile.replace("col-md-2","col-md-3");
+	var myHTMLemailTop = myHTMLemail.replace("col-md-2","col-md-3");
+	var myHTMLgithubTop = myHTMLgithub.replace("col-md-2","col-md-3");
+	var myHTMLlocationTop = myHTMLlocation.replace("col-md-2","col-md-3");
+	//top contacts
+	$("#topContacts").append(myHTMLmobileTop, myHTMLemailTop, myHTMLgithubTop, myHTMLlocationTop);
+	// footer contacts footerContacts
+	$("#footerContacts").append(myHTMLmobile, myHTMLemail, myHTMLgithub, myHTMLtwitter, myHTMLblog, myHTMLlocation);
+	// building header
+	$("#header").append(myHTMLbioPic, myHTMLwelcomeMsg);
+	$("#header").prepend(myHTMLheaderRole);
+	$("#header").prepend(myHTMLheaderName);
+
+	if (bio.skills.length > 0) {
+		$("#header").append(HTMLskillsStart);
+		bio.skills.forEach(function(s){
+			var myHTMLskills = HTMLskills.replace("%data%", s);
+			$("#skills-h3:last").append(myHTMLskills);
+		});
+	}
+};
+
 
 // education
 var education = {
 	"schools" : [
 			{
 				"name": "Good Dog University",
-				"location": "Russia",
+				"location": "são paulo - brazil",
 				"degree": "bach",
 				"majors": ["CS"],
-				"dates": 2000,
+				"dates": "2000-2005",
 				"url": "http://www.gduniv.com"
-
 			},
 			{
 				"name": "Spatial Station College",
-				"location": "Outer Space",
+				"location": "Recife - brazil",
 				"degree": "master",
 				"majors": ["MS"],
-				"dates": 2005,
+				"dates": "2005-2010",
 				"url": "http://www.outerspace.site.com"
 
 			}
 		],
-
 	"onlineCourses" : [
 			{
-				"title": "python",
-				"school": "udacity",
-				"dates": 2017,
-				"url": "http://www.udacity.com/course/"
+				"title": "Python",
+				"school": "Udacity",
+				"dates": "2017",
+				"url": "http://www.udacity.com/course/python"
 			},
 			{
-				"title": "python",
-				"school": "udacity",
-				"dates": 2017,
-				"url": "http://www.udacity.com/course/"
+				"title": "JavaScript",
+				"school": "Udacity",
+				"dates": "2017",
+				"url": "http://www.udacity.com/course/js"
 			}
-		]	
-}
+		]
+};
+education.display = function(){
+	if (education.schools.length > 0){
+		$("#education").append(HTMLschoolStart);
+		education.schools.forEach(function(school){
+			var myHTMLschoolName = HTMLschoolName.replace("%data%",school.name);
+			var myHTMLschoolDegree = HTMLschoolDegree.replace("%data%",school.degree);
+			var myHTMLschoolDates = HTMLschoolDates.replace("%data%",school.dates);
+			var myHTMLschoolLocation = HTMLschoolLocation.replace("%data%",school.location);
+			var myHTMLschoolMajor = HTMLschoolMajor.replace("%data%",school.majors);
+			$(".education-entry:last").append(myHTMLschoolName+myHTMLschoolDegree);
+			$(".education-entry:last").append(myHTMLschoolDates);
+			$(".education-entry:last").append(myHTMLschoolLocation);
+			$(".education-entry:last").append(myHTMLschoolMajor);
+		});
+		$(".education-entry:last").append(HTMLonlineClasses);
+		education.onlineCourses.forEach(function(course){
+			var myHTMLonlineTitle = HTMLonlineTitle.replace("%data%",course.title);
+			var myHTMLonlineSchool = HTMLonlineSchool.replace("%data%",course.school);
+			var myHTMLonlineDates = HTMLonlineDates.replace("%data%",course.dates);
+			var myHTMLonlineURL = HTMLonlineURL.replace("%data%",course.url);
+			$(".education-entry:last").append(myHTMLonlineTitle+myHTMLonlineSchool);
+			$(".education-entry:last").append(myHTMLonlineDates);
+			$(".education-entry:last").append(myHTMLonlineURL);
+		});
+	}
+};
+
 
 //projects
 var projects = {
-	"project" : [
+	"projects" : [
 	{"title": "Space Research of Life",
 	"dates":"2000-2010",
-	"description": "Trying to find life in the outer space",
-	"images": ["https://advocateofentropy.files.wordpress.com/2014/05/multiple-cells.jpg",
-		"https://userscontent2.emaze.com/images/e5c42ed8-aafd-49dc-bbe7-44ffa0364500/640e0690ab563144364b74fa67d6f6ae.jpg"]
+	"description": "Trying to find life in the outer space. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sem neque, maximus viverra sapien eu, dapibus vehicula leo. Aenean tempor est quis dolor dapibus finibus. In hac habitasse platea dictumst. Integer id malesuada quam. Mauris elementum risus euismod turpis convallis bibendum. Ut facilisis imperdiet pretium. Quisque commodo, leo et mattis imperdiet, quam diam viverra odio, ut sollicitudin est diam id dolor. Suspendisse potenti. Maecenas sed lacus eget orci suscipit imperdiet.",
+	"images": ["images/project01a.png", "images/project01b.png"]
 	},
 	{"title": "Ancient Aliens Computers",
 	"dates":"2010-2015",
-	"description": "Alien technology are studied behind the lens of",
-	"images": ["http://www.openminds.tv/wp-content/uploads/I-Dont-Always-Aliens.jpg",
-		"https://pogd.es/assets/bg/Ancient-Aliens.jpg"]
+	"description": "Alien technology are studied behind the lens of ancient aliens research. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sem neque, maximus viverra sapien eu, dapibus vehicula leo. Aenean tempor est quis dolor dapibus finibus. In hac habitasse platea dictumst. Integer id malesuada quam. Mauris elementum risus euismod turpis convallis bibendum. Ut facilisis imperdiet pretium. Quisque commodo, leo et mattis imperdiet, quam diam viverra odio, ut sollicitudin est diam id dolor. Suspendisse potenti. Maecenas sed lacus eget orci suscipit imperdiet.",
+	"images": ["images/project02a.png", "images/project02b.png"]
 	}]
-}
+};
+projects.display = function(){
+	if (projects.projects.length > 0){
+		projects.projects.forEach(function(project){
+			$("#projects").append(HTMLprojectStart);
+			var myHTMLprojectTitle = HTMLprojectTitle.replace("%data%",project.title);
+			var myHTMLprojectDates = HTMLprojectDates.replace("%data%",project.dates);
+			var myHTMLprojectDescription = HTMLprojectDescription.replace("%data%",project.description);
+
+			$(".project-entry:last").append(myHTMLprojectTitle);
+			$(".project-entry:last").append(myHTMLprojectDates);
+			$(".project-entry:last").append(myHTMLprojectDescription);
+
+			project.images.forEach(function(image){
+				var myHTMLprojectImage = HTMLprojectImage.replace("%data%",image);
+				$(".project-entry:last").append(myHTMLprojectImage);
+			});
+		});
+	}
+};
+
 
 //work
 var work = {
 	"jobs" : [
 		{"employer": "Ultra Spacial Security",
 		"title": "Analyst",
-		"location": "top secret location",
+		"location": "Rio Branco - Brazil",
 		"dates": "2000-2010",
 		"description": "Ultra secrets activites envolving aliens"},
 		{"employer": "Global Space",
 		"title": "Astronaut",
-		"location": "Moon",
-		"dates": "2010-",
-		"description": "Several importants activites"}]
-}
-
-// starting header
-
-	// name and role
-	var HTMLheaderName = HTMLheaderName.replace("%data%", bio["name"]);
-	var HTMLheaderRole = HTMLheaderRole.replace("%data%", "web develloper");
-	var HTMLbioPic = HTMLbioPic.replace("%data%",bio["pictureUrl"]);
-	var HTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%",bio["welcome"]);
-	// contacts
-	var HTMLmobile = HTMLmobile.replace("%data%", bio["contacts"]["mobile"]);
-	var HTMLemail = HTMLemail.replace("%data%", bio["contacts"]["email"]);
-	var HTMLgithub = HTMLgithub.replace("%data%",bio.contacts["github"]);
-	var HTMLtwitter = HTMLtwitter.replace("%data%",bio.contacts["twitter"]);
-	var HTMLlocation = HTMLlocation.replace("%data%",bio.contacts["location"]);
-	
-	$("#topContacts").append(HTMLmobile);
-	$("#topContacts").append(HTMLemail);
-	$("#topContacts").append(HTMLgithub);
-	$("#topContacts").append(HTMLtwitter);
-	$("#topContacts").append(HTMLlocation);
-
-	$("#header").append(HTMLbioPic);
-	$("#header").append(HTMLwelcomeMsg);
-	$("#header").prepend(HTMLheaderRole);
-	$("#header").prepend(HTMLheaderName);
-	
-// building HTMLskillsStart to the id=header
-if (bio["skills"].length > 0) {
-
-	$("#header").append(HTMLskillsStart);
-	// write a loop to append all skills
-	for (k=0; k<bio["skills"].length; k++){
-		var myHTMLskills = HTMLskills.replace("%data%", bio["skills"][k]);
-		$("#skills").append(myHTMLskills);
+		"location": "São Carlos - Brazil",
+		"dates": "2010-2017",
+		"description": "Several importants activites. "}]
+};
+work.display = function(){
+	if (work.jobs.length > 0){
+		work.jobs.forEach(function(job){
+			$("#workExperience").append(HTMLworkStart);
+			var myHTMLworkEmployer = HTMLworkEmployer.replace("%data%", job.employer);
+			var myHTMLworkTitle = HTMLworkTitle.replace("%data%", job.title);
+			var myHTMLworkLocation = HTMLworkLocation.replace("%data%", job.location);
+			var myHTMLworkDates = HTMLworkDates.replace("%data%", job.dates);
+			var myHTMLworkDescription = HTMLworkDescription.replace("%data%", job.description);
+			var formattedEmployerTittle = myHTMLworkEmployer + myHTMLworkTitle;
+			$(".work-entry:last").append(formattedEmployerTittle);
+			$(".work-entry:last").append(myHTMLworkLocation);
+			$(".work-entry:last").append(myHTMLworkDates);
+			$(".work-entry:last").append(myHTMLworkDescription);
+		});
 	}
-}
+};
 
-// work experience
-/*work["jobs"].forEach(function(job){
-	var HTMLworkEmployer = HTMLworkEmployer.replace("%data%", job["employer"]);
-	var HTMLworkTitle = HTMLworkTitle.replace("%data%", job["title"]);
-	$("#workExperience").append(HTMLworkEmployer);
-	$("#workExperience").append(HTMLworkTitle);	
-});
-*/
 
-for (job in work["jobs"]){
-	$("#workExperience").append(HTMLworkStart);
-	var myHTMLworkEmployer = HTMLworkEmployer.replace("%data%", work["jobs"][job]["employer"]);
-	var myHTMLworkTitle = HTMLworkTitle.replace("%data%", work["jobs"][job]["title"]);
-	var formattedEmployerTittle = myHTMLworkEmployer + myHTMLworkTitle;
-	$(".work-entry:last").append(formattedEmployerTittle);
-}
+bio.display();
+work.display();
+projects.display();
+education.display();
+$("#mapDiv").append(googleMap);
+
+var countSkilss = bio.skills.length;
+var countPlaces = 1 + education.schools.length + work.jobs.length;
+var countExperience = work.jobs.length;
+var countProjects = projects.projects.length;
+
+Highcharts.chart('graph', {  
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Counting metadata'
+    },
+    subtitle: {
+        text: '# of places, courses and others'
+    },
+    xAxis: {
+        categories: [
+            'Skills',
+            'Places',
+            'Work Experience',
+            'Projects'
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Counts'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y}</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'Counts',
+        data: [countSkilss, countPlaces, countExperience, countProjects]
+        //data: [1,2,3,4]
+    }]
+}); 
 
 /*
-HTMLworkTitle
-HTMLworkDates
-HTMLworkLocation
-HTMLworkDescription
+function locationizer(work_obj){
+    locations = [];
+    for (w in work_obj){
+        locations.push(work_obj[w]['location']);
+    }
+    return locations;
+};
+
+$('#main').append(internationalizeButton);
+var inName = function(name){
+	var first = name.split(" ")[0];
+	var second = name.split(" ")[1];
+	first = first.toLowerCase();
+	second = second.toUpperCase();
+	result = first[0].toUpperCase()+first.slice(1)+" "+second;
+	return result;
+};
 */
-
-/*
-var myName = "Dante";
-var formattedName = HTMLheaderName.replace("%data%", myName);
-
-
-var myRole = "web develloper";
-var formattedRole = HTMLheaderRole.replace("%data%", myRole);
-var skills = ["awesomeness", "programming", "teaching", "JavaScript"];
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-var genMySkills = function(){
-	for (var i = 0; i <= skills.length-1; i++) {
-		var myHTMLskills = HTMLskills.replace("%data%", skills[i]);
-		$("#header").append(myHTMLskills);
-	}
-}
-
-genMySkills()
-
-var work = {
-
-	"jobPosition": "ninja Master",
-	"employer": "Secret Service",
-	"yearsWorked": "5 years",
-	"city": "Moscow"
-}
-
-var education = {
-	"lastSchool": "NASA",
-	"yearsAttended": "3 years",
-
-}
-$("#main").append(bio.name);
-*/
-
-
+console.log([countSkilss, countPlaces, countExperience, countProjects]);
